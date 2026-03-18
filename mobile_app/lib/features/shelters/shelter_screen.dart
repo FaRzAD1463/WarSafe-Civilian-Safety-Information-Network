614f6c39016c5fdc4eb30e42b281e4c23f349640
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-18 21:09:16 +0600

    Update shelter_screen.dart

--- mobile_app/lib/features/shelters/shelter_screen.dart
+++ mobile_app/lib/features/shelters/shelter_screen.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import '../../core/services/api_service.dart';
 
-class ShelterScreen extends StatelessWidget {
+class ShelterScreen extends StatefulWidget {
+  const ShelterScreen({super.key});
+
+  @override
+  State<ShelterScreen> createState() => _ShelterState();
+}
+
+class _ShelterState extends State<ShelterScreen> {
+  List shelters = [];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  void load() async {
+    final data = await ApiService.get("shelters");
+    setState(() => shelters = data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Shelters")),
-      body: Center(child: Text("Shelter list coming soon")),
+      appBar: AppBar(title: const Text("Shelters")),
+      body: ListView.builder(
+        itemCount: shelters.length,
+        itemBuilder: (_, i) => ListTile(
+          title: Text(shelters[i]["name"]),
+        ),
+      ),
     );
   }
 }
