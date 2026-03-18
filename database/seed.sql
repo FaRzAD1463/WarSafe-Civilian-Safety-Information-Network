f18c52c3492d5fd93fde75a3031ded5b5eada5f9
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-18 20:48:42 +0600

    Add seed data for Reports table

---
+++ database/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Reports" ("Id", "Type", "Description", "Latitude", "Longitude")
+VALUES
+(gen_random_uuid(), 'Explosion', 'Loud explosion reported', 23.81, 90.41),
+(gen_random_uuid(), 'Fire', 'Building on fire', 23.82, 90.42);
