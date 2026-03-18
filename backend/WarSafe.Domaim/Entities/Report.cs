43cf44a979ce4a5e9aa7f9371beaa3e653652220
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-18 17:24:49 +0600

    Refactor Report class to include Status and update CreatedAt
    Added 'Status' property and initialized 'CreatedAt' to current UTC time.

--- backend/WarSafe.Domaim/Entities/Report.cs
+++ backend/WarSafe.Domaim/Entities/Report.cs
@@ -1,12 +1,20 @@
 namespace WarSafe.Domain.Entities;
 
+using WarSafe.Domain.Enums;
+
 public class Report
 {
     public Guid Id { get; set; }
+
     public string Type { get; set; }
+
     public string Description { get; set; }
+
     public double Latitude { get; set; }
+
     public double Longitude { get; set; }
-    public string Urgency { get; set; }
-    public DateTime CreatedAt { get; set; }
+
+    public ReportStatus Status { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
