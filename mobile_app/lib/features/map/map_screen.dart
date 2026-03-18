d1fc4bb014940ecb128c21b5cdea2920ef3db287
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-18 21:07:08 +0600

    Update map_screen.dart

--- mobile_app/lib/features/map/map_screen.dart
+++ mobile_app/lib/features/map/map_screen.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../../core/services/api_service.dart';
 
 class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
   @override
-  _MapScreenState createState() => _MapScreenState();
+  State<MapScreen> createState() => _MapState();
 }
 
-class _MapScreenState extends State<MapScreen> {
-  static const LatLng center = LatLng(31.5, 34.4);
+class _MapState extends State<MapScreen> {
+  Set<Marker> markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    loadReports();
+  }
+
+  Future<void> loadReports() async {
+    final data = await ApiService.get("reports");
+
+    final Set<Marker> loadedMarkers = {};
+
+    for (var r in data) {
+      loadedMarkers.add(
+        Marker(
+          markerId: MarkerId(r["id"]),
+          position: LatLng(r["latitude"], r["longitude"]),
+          infoWindow: InfoWindow(title: r["type"]),
+        ),
+      );
+    }
+
+    setState(() {
+      markers = loadedMarkers;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("WarSafe Map")),
+      appBar: AppBar(title: const Text("WarSafe Map")),
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: center,
-          zoom: 10,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(23.8103, 90.4125),
+          zoom: 12,
         ),
+        markers: markers,
       ),
     );
   }
