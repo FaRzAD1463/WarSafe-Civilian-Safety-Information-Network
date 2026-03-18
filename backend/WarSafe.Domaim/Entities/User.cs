96ae1155e3281e6f822e600949aefd4e481a690f
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-18 17:26:08 +0600

    Refactor User class to use Role enum and add CreatedAt

--- backend/WarSafe.Domaim/Entities/User.cs
+++ backend/WarSafe.Domaim/Entities/User.cs
@@ -1,9 +1,16 @@
 namespace WarSafe.Domain.Entities;
 
+using WarSafe.Domain.Enums;
+
 public class User
 {
     public Guid Id { get; set; }
+
     public string Email { get; set; }
+
     public string PasswordHash { get; set; }
-    public string Role { get; set; } = "User";
+
+    public Role Role { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
