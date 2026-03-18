4299a5cc82a3c44836c31edf934b011645a3f3bb
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-18 17:25:35 +0600

    Add Capacity property to Shelter class
    Added Capacity property to Shelter class.

--- backend/WarSafe.Domaim/Entities/Shelter.cs
+++ backend/WarSafe.Domaim/Entities/Shelter.cs
@@ -1,7 +1,14 @@
+namespace WarSafe.Domain.Entities;
+
 public class Shelter
 {
     public Guid Id { get; set; }
+
     public string Name { get; set; }
+
     public double Latitude { get; set; }
+
     public double Longitude { get; set; }
+
+    public int Capacity { get; set; }
 }
