453265c83a7829afd023bbc24539333aa27b48d5
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-22 22:16:25 +0600

    Update Alert.cs

--- backend/WarSafe.Domaim/Entities/Alert.cs
+++ backend/WarSafe.Domaim/Entities/Alert.cs
@@ -3,10 +3,7 @@ namespace WarSafe.Domain.Entities;
 public class Alert
 {
     public Guid Id { get; set; }
-
     public string Message { get; set; }
-
     public string Severity { get; set; }
-
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
