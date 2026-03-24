ae0dc9c3b1125186c97858c0bfd4ef60f13aad91
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-24 23:11:14 +0600

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,4 +1,5 @@
 INSERT INTO "Reports" ("Id", "Type", "Description", "Latitude", "Longitude")
 VALUES
+  
 (gen_random_uuid(), 'Explosion', 'Loud explosion reported', 23.81, 90.41),
 (gen_random_uuid(), 'Fire', 'Building on fire', 23.82, 90.42);
