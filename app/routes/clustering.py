52d1a06937f63ad01698e4a526b8d06772255786
Farzad Khan <103814290+FaRzAD1463@users.noreply.github.com>
2026-03-25 22:43:46 +0600

    Update clustering.py

--- app/routes/clustering.py
+++ app/routes/clustering.py
@@ -3,7 +3,6 @@ from app.schemas.report_schema import ClusterRequest, ClusterResponse
 from app.services.clustering_service import ClusteringService
 
 router = APIRouter()
-
 @router.post("/", response_model=ClusterResponse)
 def cluster(data: ClusterRequest):
     clusters = ClusteringService.run(data.points)
